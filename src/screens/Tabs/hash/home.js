22f2755226b92b2dfdab2a29bc519be6e4671046
Jingon-Park <bennp3038@gmail.com>
2020-08-24 12:38:18 +0900

    Hash검색창, 인기태그 및 화면 분할
    Hash검색창, 인기태그 및 화면분할 구현

--- src/screens/Tabs/hash/home.js
+++ src/screens/Tabs/hash/home.js
@@ -1,17 +1,105 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-import Styles from '../../../styles';
+import { Dimensions,ScrollView, StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
-function MemoryHome({ navigation }) {
+const winHeight = Dimensions.get('window').height;
+const winWidth  = Dimensions.get('window').width;
 
+
+const hash_rank = [{hash_rank: "동건짱"}, {hash_rank:"리그오브레전드"}, {hash_rank:"롤"}, {hash_rank:"롤"}, {hash_rank:"롤"}
+, {hash_rank:"롤"}, {hash_rank:"롤"}, {hash_rank:"롤"}, {hash_rank:"롤"}, {hash_rank:"롤"}];
+function HashHome({ navigation }) {
+    
+    
+    
     return (
-        <View style={Styles.container}>
-            <Text>Memory Home</Text>
 
+        <View style = {styles.container}>
+            <View
+                style={styles.search_bar_view}>
+                <TextInput style={styles.search_bar} placeholder="키워드를 검색하세요"
+                />
+            </View>
+            <Text style = {{
+                paddingTop : 10,
+                paddingLeft:10,
+                fontSize:14,
+                paddingBottom:5,
+
+            }}>인기 키워드</Text>
+            <ScrollView 
+                horizontal={true}
+                showsHorizontalScrollIndicator={true}
+                onMomentumScrollEnd={() => {
+                console.log('Scrolling is End');
+            }}>
+                <View
+                style = {{
+                    flexDirection:"row",
+                    height: winHeight*0.03,
+                }}>
+                {hash_rank.map((data, index)=>{
+                    return(
+                        <View
+                            style={styles.Top_Hash_view}>
+                        <Text style = {{color: "white", fontSize:13.5,fontWeight: 'bold'}}># {data.hash_rank}</Text>
+                        </View>
+                    )
+                 })}
+                </View>
+            </ScrollView>
+            <View style={styles.search_list_view}>
+                <Text>검색 키워드 목록</Text>
+            </View>
+            <View
+            style={styles.chat_list_view}>
+                <Text>chat_list_view</Text>
+            </View>
         </View>
     );
+    
 }
 
+const styles = StyleSheet.create({
+    container: {
+        height:winHeight,
+        backgroundColor:"#FFDBC1",
+        padding:5,
+    },
+    search_bar_view:{
+        flexDirection: 'row',
+            width: "100%",
+            justifyContent:"center",
+            paddingTop: 10,
+            height: 55, //%로 설정하면 검색창이 켜질때 마다 크기가 줄어든다...
+            backgroundColor: "#E1B771",
+    },
+    search_bar: {
+        fontSize: 15,
+        width: winWidth - 30,
+        borderWidth: 1.5,
+        borderColor: "#00255A",
+        borderRadius: 15,
+    },
+    Top_Hash_view:{
+        marginLeft:7,
+        backgroundColor: "#00255A",
+        borderRadius:7,
+        padding:3, //하나의 요소 내에서 테두리로부터 요소 안의 내용물까지의 간격을 의미
+        marginBottom:7,
+        height: winHeight*0.03,
+    },
+    search_list_view:{
+        paddingTop:10,
+        backgroundColor: "#3DFF92", //구분을 위한 임시 배경 컬러
+        height:winHeight*0.11,
+        paddingLeft:10
+    },
+    chat_list_view:{
+        flexDirection:"row",
+        backgroundColor: "#FFAAFF",
+        height: winHeight*0.7,
+    }
+});
 
 
-export default MemoryHome;
+export default HashHome;
--- src/screens/Tabs/hash/index.js
+++ src/screens/Tabs/hash/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Text } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack';
-import MemoryHome from './home'
+import HashHome from './home'
 import Styles from '../../../styles';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
+import { StyleSheet, TouchableOpacity, View, Image } from 'react-native';
+
 
 const Stack = createStackNavigator();
 
-export default function MemoryScreen({ navigation }) {
+export default function HashScreen({ navigation }) {
     return (
         <Stack.Navigator>
-            <Stack.Screen
+            {/* <Stack.Screen
                 name="Home"
-                component={MemoryHome}
+                component={HashHome}
                 options={{
-                    title: 'Home', headerLeft: () => (
+                    title: 'Hash Tag', headerLeft: () => (
                         <TouchableOpacity
                             style={Styles.headerButton}
                             onPress={() => navigation.openDrawer()}
@@ -22,6 +24,23 @@ export default function MemoryScreen({ navigation }) {
                             <Text>Menu</Text>
                         </TouchableOpacity>
                     ),
+                }}
+            /> */}
+            <Stack.Screen
+                name="Home"
+                component={HashHome}
+                options={{
+                    headerLeft: () => <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                        <Image
+                            source={require('../../../image/menu_1.png')} style={{ height: 50, width: 50, resizeMode: 'contain'}}
+                        />
+                    </TouchableOpacity>,
+                    headerTitle: () =>
+                        <View style={{ alignItems: "center", flex:1 }}>
+                            <Image source={require('../../../image/logo_mini_02.png')} style={{ height: 50, width: 50, resizeMode: 'contain'}}/>
+                        </View>
+                    ,
+                    headerRight: () => <Image source={require('../../../image/mail_g.png')} style={{ height:80, width:80, resizeMode: 'contain' }}/>,
                 }}
             />
 
