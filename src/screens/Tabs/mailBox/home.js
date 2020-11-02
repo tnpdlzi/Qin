7a0c90c562fd2500d721b857ed25ecc003d710a8
이동건 <tnpdlzi@naver.com>
2020-11-02 16:49:49 +0900

    모달 띄우기 완료

--- src/screens/Tabs/mailBox/home.js
+++ src/screens/Tabs/mailBox/home.js
@@ -1,7 +1,8 @@
 import React, {Component, useState, useEffect} from 'react';
-import {ScrollView, View, StyleSheet, Image, Text, TouchableOpacity, RefreshControl} from 'react-native';
+import {ScrollView, View, StyleSheet, Image, Text, TouchableOpacity, RefreshControl, TouchableHighlight} from 'react-native';
 import server from '../../../../server.json';
 import {Avatar, Accessory} from 'react-native-elements';
+import Modal from 'react-native-modal';
 import axios from 'axios';
 
 // refresh가 완료될 때 까지 wait
@@ -27,6 +28,11 @@ function MailHome({ navigation, route }) {
 
     const [datas, setDatas] = useState([]);
 
+    const [modalVisible, setModalVisible] = useState(new Array(datas.length).fill(false));
+    console.log('데이타스길이 : ' + datas.length)
+
+    let arr = new Array(modalVisible.length).fill(false);
+
     // 새로고침 onRefresh로 구현. onRefresh를 호출하면 setDatas, setMyRoom으로 hook 다시 불러옴. 그에 따라 화면도 다시 render
     const [refreshing, setRefreshing] = useState(false);
     const onRefresh = React.useCallback(async() => {
@@ -38,17 +44,6 @@ function MailHome({ navigation, route }) {
       }, []);
     
 
-    // 이거는 시간 연장할때 쓰는거
-    let refresh = async (rfurl) => await axios.get(rfurl)
-        .then(function (response) {
-            console.log(rfurl)
-        }).then(onRefresh)
-        .catch(function (error) {
-            console.log(rfurl)
-            console.log('error : ' + error);
-        });
-
-        // 이 네비게이션이 focus되면 실행. onRefresh()를 실행해주겠다! 이걸로 자동 새로고침이 됨
         useEffect(() => {
             const unfetched = navigation.addListener('focus', async() => {
                 setDatas(await getDatas(server.ip + '/mail/getMails?&uID=1'))
@@ -67,10 +62,25 @@ function MailHome({ navigation, route }) {
                       }>
                     {datas.map((data, index) => {
 
+                        console.log('index값 : ' + index)
+
                         // 서버 시간과의 차이를 한국 시간으로 바꿔줌
                         let rdate = new Date(data.createdTime);
                         rdate.setHours(rdate.getHours() + 9);
                         let endtime = rdate.toString().substr(16, 5);
+                        let game = '';
+
+                        if(data.game == 'LOL'){
+                            game = 'LEAGUE OF LEGENDS';
+                        }else if(data.game == 'OW'){
+                            game = 'OVERWATCH';
+                        }else if(data.game == 'BG'){
+                            game = 'BATTLEGROUND';
+                        }else if(data.game == 'RS'){
+                            game = 'RAINBOW SIX SIEGE';
+                        }else{
+                            game = data.game;
+                        }
 
                         return (
                             <View style={styles.sItem} >
@@ -80,8 +90,10 @@ function MailHome({ navigation, route }) {
                                         justifyContent: 'space-between',
                                         width: '100%',
                                     }}
-                                    // onPress={async () => navigation.navigate('joinedLOL', {memtitle: [await getDatas(server.ip + '/category/member?roomID=' + data.roomID + '&game=LOL'), await getDatas(server.ip + '/category/title?roomID=' + data.roomID), data.roomID, await getDatas(server.ip + '/category/ismember?roomID=' + data.roomID + '&uID=1')]})}>
-                                    >
+                                    onPress={() => {
+                                        arr[index]=true;
+                                        setModalVisible(arr);
+                                    }}>
 
                                         <View
                                             style={{
@@ -142,6 +154,107 @@ function MailHome({ navigation, route }) {
                                         </Text>
                                     </View>
                                 </TouchableOpacity>
+
+                                <Modal
+                                    animationIn={"slideInUp"} //default 'slideInUp'
+                                    animationOut={'slideOutDown'} //default 'slideOutDown'
+                                    isVisible={modalVisible[index]}
+                                    transparent={true} //default 'true'
+                                    backdropColor={'black'} //default 'black'
+                                    backdropOpacity={0.5} //default 0.7
+                                    onBackButtonPress={() => {
+                                        arr[index]=false;
+                                        setModalVisible(arr);
+                                    }}
+                                    onBackdropPress={() => setModalVisible(false)}
+                                >
+                                    <View style={styles.centeredView}>
+                                        <View style={styles.modalView}>
+                                            <TouchableOpacity onPress={() => { 
+                                                arr[index]=false;
+                                                setModalVisible(arr);
+                                            }}
+                                            >
+                                                <Image style={{ width: 70, height: 70, }} source={require('../../../image/cancel.png')} />
+                                            </TouchableOpacity>
+                                            <View style={{
+                                                width: '70%', height: '15%', alignSelf: 'center',
+                                                flexDirection: 'row', marginBottom: 20
+                                            }}>
+                                                <View style={{ width: '70%', }}>
+                                                    <Text style={{ fontSize: 16, fontWeight: 'bold', marginTop: '5%' }}>{data.userName}</Text>
+                                                    <Text style={{ fontSize: 12, marginTop: '3%' }}>{data.intro}</Text>
+                                                </View>
+                                                <View style={{ width: '30%', alignItems: 'center' , justifyContent: 'flex-start', marginTop: '5%'}}>
+                                                    <Avatar
+                                                        rounded
+                                                        style={{ width: '70%', height: '70%'}}
+                                                        source={require('../../../image/profile.png')}
+                                                    />
+                                                </View>
+                                            </View>
+                                            
+                                            <View style={{ width: '70%', height: 1, backgroundColor: "#E2E2E2", alignSelf: "center" }} />
+                                            
+                                            <View style={{ width: '70%', height: '10%', alignSelf: 'center', flexDirection: 'row', paddingTop: 20 }}>
+                                                <View style={{ width: '50%', flexDirection: 'row', alignItems: 'center', }}>
+                                                    <Text style={{ color: '#363636', fontSize: 12, fontWeight: 'bold' }}>매너 지수   </Text>
+                                                    <Text style={{ color: '#FFC81A', fontSize: 12, fontWeight: 'bold' }}>{data.good}</Text>
+                                                </View>
+                                                <View style={{ width: '50%', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
+                                                    <Text style={{ color: '#363636', fontSize: 12, fontWeight: 'bold' }}>비매너 지수   </Text>
+                                                    <Text style={{ color: '#00255A', fontSize: 12, fontWeight: 'bold' }}>{data.bad}</Text>
+                                                </View>
+                                            </View>
+                                            
+                                            
+                                            <View style={{ width: '70%', alignSelf: 'center', paddingTop: 20, marginBottom: 10 }}>
+                                                
+                                                <View style={{height: 100, justifyContent:'center'}} >
+                                                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                                        <Text style={{ fontSize: 20, color: '#FFC81A' }}>{'\u2022 '} </Text>
+                                                        <Text style={{ fontSize: 14, fontWeight: 'bold' }}>{game}</Text>
+                                                    </View>
+                                                    <View style={{ flexDirection: 'row', alignItems: 'center', paddingBottom: 20 }}>
+                                                        <Text style={{ fontSize: 20, color: '#A5A5A5' }}>{'\u2022 '} </Text>
+                                                        <Text style={{ fontSize: 12 }}>{data.gameID}</Text>
+                                                        <Text style={{ fontSize: 12 }}> </Text>
+                                                        <Text style={{ fontSize: 12 }}>({data.tierID})</Text>
+                                                    </View>
+                                                </View>
+                                                
+                                            </View>
+                                            
+                                            <View style={{ width: '80%', height: 1, backgroundColor: "#E2E2E2", alignSelf: "center" }} />
+                                            
+                                            
+
+                                            <View style={{width:'100%', flexDirection:'row', marginTop: 20, justifyContent:'center'}}>
+                                                <TouchableOpacity
+                                                    style={{
+                                                        width: '45%', height: 20, alignSelf: 'center'
+                                                        , justifyContent: 'center'
+                                                    }}
+                                                    onPress={() => { }}>
+                                                    <Text style={{ color: "gray", fontWeight: "bold", textAlign: "center", fontSize: 16 }}>싫어요</Text>
+                                                </TouchableOpacity>
+
+                                                <View style={{ width: 1, height: 40, backgroundColor: "#E2E2E2", alignSelf: "center" }} />
+
+                                                <TouchableOpacity
+                                                    style={{
+                                                        width: '45%', height: 20, alignSelf: 'center'
+                                                        , justifyContent: 'center'
+                                                    }}
+                                                    onPress={() => { }}>
+                                                    <Text style={{ color: "black", fontWeight: "bold", textAlign: "center", fontSize: 16 }}>좋아요</Text>
+                                                </TouchableOpacity>
+                                            </View>
+                                            
+
+                                        </View>
+                                    </View>
+                                </Modal>
                             </View>
                         );
                     })}
@@ -170,6 +283,27 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'flex-start',
         alignItems: 'center',
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 0,
+    },
+    modalView: {
+        width: '90%',
+        height: 410,
+        backgroundColor: "white",
+        borderRadius: 20,
+        alignItems: 'flex-start',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
     },
 });
 
