8381bedf99b34c8405b12ca00269947343d34e22
이동건 <tnpdlzi@naver.com>
2020-11-09 19:01:55 +0900

    로그인 토스트 띄우기

--- src/screens/login/home.js
+++ src/screens/login/home.js
@@ -1,5 +1,5 @@
 import React, {Component, useState, useEffect} from 'react';
-import {View, Text, Button, TouchableOpacity, Image, StyleSheet, TextInput, ScrollView} from 'react-native';
+import {View, Text, Button, TouchableOpacity, Image, StyleSheet, TextInput, ScrollView, ToastAndroid} from 'react-native';
 
 import join from './join/join';
 import find_id from './find_id/find_id';
@@ -9,6 +9,12 @@ import axios from 'axios';
 import { NavigationActions } from 'react-navigation';
 import CategoryScreen from '../Tabs/index';
 
+const showPWToast = () => {
+    ToastAndroid.show("비밀번호가 일치하지 않습니다.", ToastAndroid.SHORT);
+  };
+const showIDToast = () => {
+    ToastAndroid.show("아이디가 일치하지 않습니다.", ToastAndroid.SHORT);
+  };
 
 const qs = require('qs');
 
@@ -173,17 +179,25 @@ function LoginHome({ navigation }) {
                     로그인 상태 유지
                 </Text>
                 <TouchableOpacity onPress={async() => {
-                    userDatas = await getDatas(server.ip + '/users/login?userID=' + userID + '&password=' + password)
+                    userDatas = await getDatas(server.ip + '/users/login?userID=' + userID + '&password=' + password).catch("false")
+                    console.log('userDatas 입니다....' + userDatas)
                     // asyncstorage 구현 파트
 
-                    console.log("이것은 uID입니다..." + userDatas.uID)
-                    console.log("이것은 userID입니다..." + userDatas.userID)
-                    console.log("이것은 userName입니다..." + userDatas.userName)
-                    console.log("이것은 good입니다..." + userDatas.good)
-                    console.log("이것은 bad입니다..." + userDatas.bad)
-                    console.log("이것은 intro입니다..." + userDatas.intro)
+                    // console.log("이것은 uID입니다..." + userDatas.uID)
+                    // console.log("이것은 userID입니다..." + userDatas.userID)
+                    // console.log("이것은 userName입니다..." + userDatas.userName)
+                    // console.log("이것은 good입니다..." + userDatas.good)
+                    // console.log("이것은 bad입니다..." + userDatas.bad)
+                    // console.log("이것은 intro입니다..." + userDatas.intro)
                     
-                    navigation.navigate(CategoryScreen)
+                    if(userDatas == false){
+                        showPWToast()
+                    } else if(userDatas == undefined){
+                        showIDToast()
+                    }
+                     else{
+                        navigation.navigate(CategoryScreen)
+                    }
                     
                     }}>
                 <Text  style={{fontSize: 15, color: '#f8f6f6', paddingVertical: 5,paddingLeft: 40, paddingRight: 40, backgroundColor: textB,borderStyle: 'solid',paddingTop: 10, paddingBottom: 10,
