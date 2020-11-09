951e45602d709fb841f08da8ae85d614453c8643
이동건 <tnpdlzi@naver.com>
2020-11-09 18:07:56 +0900

    로그인 정보받기 구현

--- src/screens/login/home.js
+++ src/screens/login/home.js
@@ -4,32 +4,49 @@ import Styles from '../../styles';
 import join from './join/join';
 import find_id from './find_id/find_id';
 import find_pw from './find_pw/find_pw';
+import server from '../../../server.json';
+import axios from 'axios';
+import { NavigationActions } from 'react-navigation';
+import CategoryScreen from '../Tabs/index';
 
-import CategoryScreen from '../Tabs/index'
 
+const qs = require('qs');
+
+let getDatas = async (url) => await axios.get(url)
+    .then(function (response) {
+        console.log(response.data)
+        return response.data
+    })
+    .catch(function (error) {
+        console.log(url)
+        console.log('error : ' + error);
+    });
 
 // 10월21일부
 function LoginHome({ navigation }) {
 
-
-
     const [logIn, setLogIn] = useState(false);
-    let [textC, changeTextC] = useState(require('../../image/login_unchecked.png'));
+    let [textC, changeTextC] = useState(require('../../../src/image/login_unchecked.png'));
+
+    const [userID, setUserID] = useState('');
+    const [password, setPassword] = useState('');
+    let userDatas;
+
     useEffect (()=>{
         if(logIn){
-            changeTextC(require('../../image/login_checked.png'));
+            changeTextC(require('../../../src/image/login_checked.png'));
         }else{
-            changeTextC(require('../../image/login_unchecked.png'))
+            changeTextC(require('../../../src/image/login_unchecked.png'))
         }
     }, [logIn]);
 
-    let [textA, changeTextA] = useState(require('../../image/logo_blue.png'));
+    let [textA, changeTextA] = useState(require('../../../src/image/logo_blue.png'));
     function setNewTextA(Text){
         if(Text == ""){
-            changeTextA(require('../../image/logo_blue.png'));
+            changeTextA(require('../../../src/image/logo_blue.png'));
         }
         else {
-            changeTextA(require('../../image/logo_yellow.png'));
+            changeTextA(require('../../../src/image/logo_yellow.png'));
         }
     }
 
@@ -90,12 +107,12 @@ function LoginHome({ navigation }) {
                             width: 50,
                             resizeMode: 'center',
                         }}
-                        source={require('../../image/id_g.png')}
+                        source={require('../../../src/image/id_g.png')}
                     />
                     <TextInput maxLength={25}
                         style={{fontSize: 16, color: '#A5A5A5', paddingLeft: 15,}}
                         placeholder="이메일"
-                        onChangeText = {(text) => setNewTextA(text)}
+                        onChangeText = {async(text) => {setNewTextA(text), await setUserID(text)}}
                     />
                 </View>
 
@@ -120,12 +137,12 @@ function LoginHome({ navigation }) {
                             width: 50,
                             resizeMode: 'center',
                         }}
-                        source={require('../../image/pw_g.png')}
+                        source={require('../../../src/image/pw_g.png')}
                     />
                     <TextInput maxLength={14}
                         style={{fontSize: 16, color: '#A5A5A5', paddingLeft: 15,}}
                         placeholder="비밀번호" secureTextEntry={true}
-                        onChangeText = {(text) => setNewTextB(text)}
+                        onChangeText = {async(text) => {setNewTextB(text), await setPassword(text)}}
                     />
                 </View>
 
@@ -155,7 +172,20 @@ function LoginHome({ navigation }) {
                 <Text  style={{fontSize: 10, color: '#504d4d', paddingVertical: 5,paddingLeft: 10, paddingRight: 60,}}>
                     로그인 상태 유지
                 </Text>
-                <TouchableOpacity onPress={() => navigation.navigate(CategoryScreen)}>
+                <TouchableOpacity onPress={async() => {
+                    userDatas = await getDatas(server.ip + '/users/login?userID=' + userID + '&password=' + password)
+                    // asyncstorage 구현 파트
+
+                    console.log("이것은 uID입니다..." + userDatas.uID)
+                    console.log("이것은 userID입니다..." + userDatas.userID)
+                    console.log("이것은 userName입니다..." + userDatas.userName)
+                    console.log("이것은 good입니다..." + userDatas.good)
+                    console.log("이것은 bad입니다..." + userDatas.bad)
+                    console.log("이것은 intro입니다..." + userDatas.intro)
+                    
+                    navigation.navigate(CategoryScreen)
+                    
+                    }}>
                 <Text  style={{fontSize: 15, color: '#f8f6f6', paddingVertical: 5,paddingLeft: 40, paddingRight: 40, backgroundColor: textB,borderStyle: 'solid',paddingTop: 10, paddingBottom: 10,
                     borderRadius: 30,
                     borderColor: '#898585',
